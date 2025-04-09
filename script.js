6fdbf064f75de2d4821bfda3df0e938a6986782d
cel004 <ngcelinaa@gmail.com>
2025-04-08 23:24:01 -0400

    Create scroll bar

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ window.onscroll = function(){
 setInterval(function(){
     vid.currentTime = window.pageYOffset/400;
 }, 40);
+
+window.addEventListener("DOMContentLoaded", () => {
+  const scroller = document.querySelector(".scroller");
+  const scrollList = scroller.querySelector(".scroll-list");
+
+  const clone = scrollList.cloneNode(true);
+  clone.setAttribute("aria-hidden", "true");
+  scroller.appendChild(clone);
+});
