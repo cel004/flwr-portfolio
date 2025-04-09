a846b9c8122bb431b97200df3c72a2ee36c335bf
cel004 <ngcelinaa@gmail.com>
2025-04-09 00:44:16 -0400

    Create contact section

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 var vid = document.getElementById('scrollVideo');
 
-// Play the video when user scrolls
 window.onscroll = function () {
-  vid.play();  // Ensure the video plays when user scrolls
+  vid.play();
 };
 
-// Update video current time based on scroll
 setInterval(function () {
   vid.currentTime = window.pageYOffset / 400;
 }, 40);
 
-// Clone scroll list functionality (this part remains the same)
 window.addEventListener("DOMContentLoaded", () => {
   const scroller = document.querySelector(".scroller");
   const scrollList = scroller.querySelector(".scroll-list");
